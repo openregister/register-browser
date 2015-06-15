a73aaefe7e47b67ca580decd96db1825343f511b
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-06-15 17:22:30 +0100

    render markdown, remove unused routes

--- src/register_browser/routes/home.clj
+++ src/register_browser/routes/home.clj
@@ -3,14 +3,9 @@
             [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :refer [ok]]
             [clojure.java.io :as io]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [markdown.core :refer [md-to-html-string]]))
 
-(defn home-page []
-  (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
-
-(defn about-page []
-  (layout/render "about.html"))
 
 (defn entry-page [register record history]
   (layout/render
@@ -22,17 +17,21 @@
 (defn render-curie [register value]
   (str "http://" register ".openregister.org/" register "/" value ".json"))
 
+(defn render-as [field value]
+  (condp = (:datatype field)
+    "text" (md-to-html-string value)
+    value))
+
 (defn render-entry [entry]
   (into {}
         (for [[field-keyword value] entry
               :let [field-name (name field-keyword)]
-              :let [field (json/parse-stream (clojure.java.io/reader (render-curie "field" field-name)))]
-              :let [datatype (:datatype field)]]
-          [field-name value])))
+              :let [field (:entry (json/parse-stream (clojure.java.io/reader (render-curie "field" field-name)) true))]]
+          [field-name (render-as field value)])))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page))
+
+
   (GET "/:register/hash/:hash" [register hash]
        (let [json-url (str "http://" register ".openregister.org/hash/" hash ".json")
              record   (json/parse-string (slurp json-url) true)]
