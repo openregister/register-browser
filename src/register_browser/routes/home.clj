9f6c93a755e2b70945a401fc8a34cc2e452b6c2d
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-06-15 14:52:03 +0100

    convert templates from main app

--- src/register_browser/routes/home.clj
+++ src/register_browser/routes/home.clj
@@ -11,7 +11,30 @@
 (defn about-page []
   (layout/render "about.html"))
 
+(defn entry-page [register record history]
+  (layout/render
+   "entry.html" {:register register
+                 :record   record
+                 :history  [{:hash "1234" :timestamp (org.joda.time.DateTime.)}]
+                 :representations [{:name "ttl" :url "foo.ttl"}]}))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (GET "/:register/hash/:hash" [register hash]
+       (entry-page {:friendly-name "Public body" :copyright "copyright bar"}
+                   {:name "Cabinet office"
+                    :fields ["name" "value"]
+                    :last-updated (org.joda.time.DateTime.)
+                    :latest-version-url (str "/" register "/" "fakevalue")
+                    :hash hash}
+                   []))
+  (GET "/:register/:value" [register value]
+       (entry-page {:friendly-name "Public body" :copyright "copyright foo"}
+                   {:name "Cabinet office"
+                    :fields ["name" "value"]
+                    :last-updated (org.joda.time.DateTime.)
+                    :latest-version-url (str "/" register "/" value)
+                    :hash "abcdef123456"}
+                   [])))
 
